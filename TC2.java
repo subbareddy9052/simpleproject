691fdb59f48f8059528ee10cdfcc48e1aaed244a
subbareddy9052 <subbufantastic9052@gmail.com>
2020-04-01 17:08:03 +0530

    added TC3 and modified TC2

--- TC2.java
+++ TC2.java
@@ -1,2 +1,7 @@
 //This is a new test case 
 //This test case will editing new testcase
+// this is code added newly
+
+
+//this is code added newly
+//Thisis code added newly
---
+++ src/com/simple/project/TC3.java
@@ -0,0 +1,10 @@
+package com.simple.project;
+
+public class TC3 {
+
+	public static void main(String[] args) {
+		System.out.println("subbareddy");
+
+	}
+
+}
