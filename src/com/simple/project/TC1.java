3b97855c74e9784c186fb98a1f4f7d4c94ea2e52
subbareddy9052 <subbufantastic9052@gmail.com>
2020-04-01 16:52:42 +0530

    first round of commit...

---
+++ src/com/simple/project/TC1.java
@@ -0,0 +1,20 @@
+package com.simple.project;
+
+public class TC1 {
+
+	public static void main(String[] args) {
+		
+		String s="hungry";
+				if(s=="hungry"){
+					System.out.println("Eat something");
+				}
+				else{
+					System.out.println("Do your own work");
+				}
+			}
+
+		}
+
+	
+
+
